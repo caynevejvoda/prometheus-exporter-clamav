a794d77cd63895a60122ae5cff8a837cc0284865
nakashi <nakashi@noreply.codeberg.org>
2022-02-21 13:36:50 +0100

    fix UnicodeDecodeError

--- clamav_exporter.py
+++ clamav_exporter.py
@@ -2,12 +2,7 @@ import dns.resolver
 import subprocess
 
 
-server_answer = dns.resolver.resolve('current.cvd.clamav.net', 'TXT').response.answer[0]
-
-server_answer = str(server_answer)
-
-server_answer = server_answer.replace('"', ':')
-server_answer = server_answer.split(':')
+server_answer = str(dns.resolver.resolve('current.cvd.clamav.net', 'TXT').response.answer[0]).replace('"', ':').split(':')
 
 server_clamav_version = server_answer[1]
 server_daily_database_version = server_answer[3]
@@ -15,33 +10,23 @@ server_main_database_version = server_answer[2]
 server_bytecode_database_version = server_answer[8]
 
 
-local_answer = subprocess.check_output('clamscan --version', shell=True)
+local_answer = str(subprocess.check_output('clamscan --version', shell=True)).replace(' ', '/').split('/')
 
-local_answer = str(local_answer)
-
-local_answer = local_answer.replace(' ', '/')
-local_answer = local_answer.split('/')
+local_answer = local_answer.replace(' ', '/').split('/')
 
 local_clamav_version = local_answer[1]
 local_daily_database_version = local_answer[2]
 
-main_database_file = open('/var/lib/clamav/main.cvd','r')
-local_main_database_version = main_database_file.readline()
+main_database_file = open('/var/lib/clamav/main.cvd','r', errors='replace')
+local_main_database_version = main_database_file.readline().split(':')[2]
 main_database_file.close()
 
-local_main_database_version = local_main_database_version.split(':')
-local_main_database_version = local_main_database_version[2]
-
-bytecode_database_file = open('/var/lib/clamav/bytecode.cvd','r')
-local_bytecode_database_version = bytecode_database_file.readline()
+bytecode_database_file = open('/var/lib/clamav/bytecode.cvd','r', errors='replace')
+local_bytecode_database_version = bytecode_database_file.readline().split(':')[2]
 bytecode_database_file.close()
 
-local_bytecode_database_version = local_bytecode_database_version.split(':')
-local_bytecode_database_version = local_bytecode_database_version[2]
 
-
-datei = open('clamav_server_database_version.prom','w')
-datei.write(
+print(
     "# HELP server_clamav_version Version of ClamAV from ClamAV server.\n" + 
     "# TYPE server_clamav_version gauge\n" + 
     "server_clamav_version " + server_clamav_version + "\n" + 
@@ -74,8 +59,3 @@ datei.write(
     "# TYPE local_bytecode_database_version gauge\n" + 
     "local_bytecode_database_version " + local_bytecode_database_version
     )
-datei.close()
-
-datei = open('clamav_server_database_version.prom','r+')
-print(datei.read())
-datei.close()
